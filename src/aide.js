364ed589885d9c3caf0e684d612654ec42ea6eea
Sam Howie <samhowie@gmail.com>
2014-03-27 22:55:52 +1100

    fix(general): fix typo issues in sync util functions

--- src/aide.js
+++ src/aide.js
@@ -24,122 +24,125 @@ function createOrchestrator () {
     return new Orchestrator();
 }
 
-module.exports = {
-    loadTasks: function(fn) {
-        fn(this);
-    },
+var aide = module.exports = {};
 
-    task: function(name, deps, fn) {
-        if (arguments.length > 1) {
-            if (typeof deps === "function") {
-                fn = deps;
-                deps = undefined;
-            }
-            tasks[name] = {
-                name: function() {
-                    return name;
-                },
-                deps: function() {
-                    return deps;
-                },
-                run: function () {
-                    return aide.run(name);
-                },
-                addTo: function(orchestrator) {
-                    var i, task;
-                    orchestrator.add(name, deps, fn);
-                    if (deps != null) {
-                        for (i = 0; i < deps.length; i += 1) {
-                            task = aide.task(deps[i]);
-                            task.addTo(orchestrator); // todo: check for circular refs?
-                        }
+aide.loadTasks= function(fn) {
+    fn(this);
+};
+
+aide.task = function(name, deps, fn) {
+    if (arguments.length > 1) {
+        if (typeof deps === "function") {
+            fn = deps;
+            deps = undefined;
+        }
+        tasks[name] = {
+            name: function() {
+                return name;
+            },
+            deps: function() {
+                return deps;
+            },
+            run: function () {
+                return aide.run(name);
+            },
+            addTo: function(orchestrator) {
+                var i, task;
+                orchestrator.add(name, deps, fn);
+                if (deps != null) {
+                    for (i = 0; i < deps.length; i += 1) {
+                        task = aide.task(deps[i]);
+                        task.addTo(orchestrator); // todo: check for circular refs?
                     }
                 }
             }
         }
-        return tasks[name];
-    },
-
-    run: function(name) {
-        var deferred, task, orchestrator;
-
-        deferred = Q.defer();
-
-        orchestrator = createOrchestrator();
-        orchestrators.push(orchestrator);
-
-        task = aide.task(name);
-
-        if (task == null) {
-            aide.say("But I don't know how to " + name + "...", "error");
-            throw new Error("attempted to run unregistered task: " + name);
-        }
-
-        task.addTo(orchestrator);
-
-        orchestrator.on("task_start", function(e) {
-            aide.say("Running task " + e.task);
-        });
-
-        orchestrator.on("stop", function(e) {
-            orchestrators.splice(orchestrators.indexOf(orchestrator), 1);
-            deferred.resolve();
-        });
-
-        orchestrator.on("err", function(err) {
-            orchestrators.splice(orchestrators.indexOf(orchestrator), 1);
-            deferred.reject(err);
-        });
-
-        orchestrator.start(name);
-
-        return deferred.promise;
-    },
-
-    stop: function() {
-        var i;
-        for (i = 0; i < orchestrators.length; i += 1) {
-            orchestrators[i].stop();
-        }
-        orchestrators.length = 0;
-        return this;
-    },
-
-    say: function(msg, tone) {
-        console.log(aide.quote(msg, tone));
-    },
-
-    quote: function(msg, tone) {
-        var color, verb;
-
-        color = tones[tone];
-        if (color == null) {
-            color = tones.default;
-        }
-
-        verb = verbs[tone];
-        if (verb == null) {
-            verb = verbs.default;
-        }
-
-        return ("Aide " + verb + ", \"" + msg + "\"")[color];
-    },
-
-    cli: function() {
-        var path            = require("path");
-        var parseArgs       = require("minimist");
-        var argv            = parseArgs(process.argv.slice(2));
-
-        var aidefile        = path.join(process.cwd(), "aidefile");
-
-        require(aidefile);
-        aide.run(argv._[0]).fail(onError);
-
-        function onError (err) {
-            aide.say(err.message, "error");
-        }
-    },
-
-    util: util
+    }
+    return tasks[name];
 };
+
+aide.run = function(name) {
+    var deferred, task, orchestrator;
+
+    deferred = Q.defer();
+
+    orchestrator = createOrchestrator();
+    orchestrators.push(orchestrator);
+
+    task = aide.task(name);
+
+    if (task == null) {
+        aide.say("But I don't know how to " + name + "...", "error");
+        throw new Error("attempted to run unregistered task: " + name);
+    }
+
+    task.addTo(orchestrator);
+
+    orchestrator.on("task_start", function(e) {
+        aide.say("Running task " + e.task);
+    });
+
+    orchestrator.on("stop", function(e) {
+        orchestrators.splice(orchestrators.indexOf(orchestrator), 1);
+        deferred.resolve();
+    });
+
+    orchestrator.on("err", function(err) {
+        orchestrators.splice(orchestrators.indexOf(orchestrator), 1);
+        deferred.reject(err);
+    });
+
+    orchestrator.start(name);
+
+    return deferred.promise;
+};
+
+aide.stop = function() {
+    var i;
+    for (i = 0; i < orchestrators.length; i += 1) {
+        orchestrators[i].stop();
+    }
+    orchestrators.length = 0;
+    return this;
+};
+
+aide.say = function(msg, tone) {
+    console.log(aide.quote(msg, tone));
+};
+
+aide.quote = function(msg, tone) {
+    var color, verb;
+
+    color = tones[tone];
+    if (color == null) {
+        color = tones.default;
+    }
+
+    verb = verbs[tone];
+    if (verb == null) {
+        verb = verbs.default;
+    }
+
+    return ("Aide " + verb + ", \"" + msg + "\"")[color];
+};
+
+aide.cli = function() {
+    var path            = require("path");
+    var parseArgs       = require("minimist");
+    var argv            = parseArgs(process.argv.slice(2));
+
+    var basedir, aidefilepath;
+
+    basedir = process.cwd();
+    aidefilepath = path.resolve(basedir, "aidefile");
+
+    require(aidefilepath);
+    aide.run(argv._[0]).fail(onError);
+
+    function onError (err) {
+        aide.say(err.message, "error");
+    }
+};
+
+aide.util = util;
 
--- src/util.js
+++ src/util.js
@@ -1,85 +1,121 @@
 "use strict";
 
-var fs              = require("fs");
-var Q               = require("q");
-var minimatch       = require("minimatch");
+var fs          = require("fs");
+var path        = require("path");
+var Q           = require("q");
+var minimatch   = require("minimatch");
 
-module.exports = {
-    forEachSeries: function (arr, iterator) {
-        return util.series(arr.map(function(el) {
-            return function() {
-                return iterator(el);
+var util = module.exports = {};
+
+util.forEachSeries =  function (arr, iterator) {
+    return util.series(arr.map(function(el) {
+        return function() {
+            return iterator(el);
+        }
+    }));
+};
+
+util.series = function (arr) {
+    var promise = Q();
+
+    arr.forEach(function(el) {
+        promise = promise.then(
+            function() {
+                return el.apply(el, arguments);
             }
-        }));
-    },
+        )
+    });
 
-    series: function (arr) {
-        var promise = Q();
+    return promise;
+};
 
-        arr.forEach(function(el) {
-            promise = promise.then(
-                function() {
-                    return el.apply(el, arguments);
-                }
-            )
-        });
+util.runSequentialTasks = function (arr) {
+    return util.forEachSeries(arr, function (task) {
+        return aide.run(task);
+    });
+};
 
-        return promise;
-    },
+// walk algorithms from:
+// http://stackoverflow.com/questions/5827612/node-js-fs-readdir-recursive-directory-search
+util.walk = function (dir) {
+    var deferred, results;
 
-    runSequentialTasks: function(arr) {
-        return util.forEachSeries(arr, function (task) {
-            return aide.run(task);
-        });
-    },
+    deferred = Q.defer();
+    results = [];
 
-    walk: function(dir, done) {
-        var results = [];
-        fs.readdir(dir, function(err, list) {
-            if (err) return done(err);
-            var pending = list.length;
-            if (!pending) return done(null, results);
-            list.forEach(function(file) {
-                file = dir + "/" + file;
-                fs.stat(file, function(err, stat) {
-                    if (stat && stat.isDirectory()) {
-                        util.walk(file, function(err, res) {
-                            results = results.concat(res);
-                            if (!--pending) done(null, results);
-                        });
-                    } else {
-                        results.push(file);
-                        if (!--pending) done(null, results);
+    fs.readdir(dir, function(err, list) {
+        var pending;
+
+        if (err) {
+            deferred.reject(err);
+        }
+
+        pending = list.length;
+        if (!pending) {
+            deferred.resolve(results);
+        }
+
+        list.forEach(function(file) {
+            file = path.resolve(dir, file);
+            fs.stat(file, function(err, stat) {
+                if (stat && stat.isDirectory()) {
+                    util.walk(file).then(function(res){
+                        results = results.concat(res);
+                        if (!--pending) {
+                            deferred.resolve(results);
+                        }
+                    }, function (err) {
+                        deferred.reject(err);
+                    });
+                } else {
+                    results.push(file);
+                    if (!--pending) {
+                        deferred.resolve(results);
                     }
-                });
+                }
             });
         });
-    },
+    });
 
-    glob: function(patterns, dir) {
-        var deferred, i;
+    return deferred.promise;
+};
 
-        dir = dir || process.cwd();
+util.walkSync = function (dir) {
+    var results = [];
+    var list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        file = dir + '/' + file;
+        var stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) {
+            results = results.concat(util.walkSync(file));
+        } else {
+            results.push(file);
+        }
+    });
+    return results;
+};
 
-        deferred = Q.defer();
-
-        util.walk(dir, function(err, files) {
-            if (err != null) {
-                aide.say(err.message, "error");
-                deferred.reject(err);
-            } else {
-                files = files.map(function(el) {
-                    return el.replace(dir + "/", "");
-                });
-
-                for (i = 0; i < patterns.length; i += 1) {
-                    files = files.filter(minimatch.filter(patterns[i]));
-                }
-
-                deferred.resolve(files);
-            }
+util.glob = function (patterns, dir) {
+    var i;
+    dir = dir || process.cwd();
+    return util.walk(dir).then(function(files) {
+        files = files.map(function(el) {
+            return el.replace(dir + "/", "");
         });
+        for (i = 0; i < patterns.length; i += 1) {
+            files = files.filter(minimatch.filter(patterns[i]));
+        }
+        return files;
+    });
+};
 
-        return deferred.promise;
+util.globSync = function (patterns, dir) {
+    var files, i;
+    files = util.walkSync(dir).map(function(el) {
+        return el.replace(dir + "/", "");
+    });
+    for (i = 0; i < patterns.length; i += 1) {
+        files = files.filter(minimatch.filter(patterns[i]));
     }
+    return files;
 };
